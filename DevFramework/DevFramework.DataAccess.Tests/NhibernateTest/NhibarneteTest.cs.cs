1546fa6df27f967fe61a58699fbe95ae0ab3c350
Batuhanls <batuhanbsls06@gmail.com>
2023-12-19 22:02:25 +0300

    First Commit ->  Creating Entities,DataAccess,Business layerBusinessTests,DataAccessTests.

---
+++ DevFramework/DevFramework.DataAccess.Tests/NhibernateTest/NhibarneteTest.cs.cs
@@ -0,0 +1,36 @@
+﻿using DevFramework.Northwind.DataAccess.Concrete.EntityFramework;
+using DevFramework.Northwind.DataAccess.Concrete.Nhibernate;
+using DevFramework.Northwind.DataAccess.Concrete.Nhibernate.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DevFramework.DataAccess.Tests.NhibernateTest
+{
+	[TestClass]
+	public class EntityFrameworkTest
+	{
+		[TestMethod]
+		public void Get_all_returns_products()
+		{
+
+			NhProductDal nhProductDal = new NhProductDal(new SqlServerHelper());
+
+			var result = nhProductDal.GetList();
+
+			Assert.AreEqual(78, result.Count);
+		}
+
+
+		[TestMethod]
+		public void Get_all_with_paramater_returns_filtered_products()
+		{
+
+			NhProductDal nhProductDal = new NhProductDal(new SqlServerHelper());
+
+			var result = nhProductDal.GetList(p => p.ProductName.Contains("ab"));
+
+			Assert.AreEqual(4, result.Count);
+		}
+
+	}
+}
