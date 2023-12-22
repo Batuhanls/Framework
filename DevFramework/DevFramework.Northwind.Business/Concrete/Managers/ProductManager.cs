099ee8738c6919cb820930d7c087990d928d09a6
Batuhanls <batuhanbsls06@gmail.com>
2023-12-23 01:10:46 +0300

    Creating log and cache aspects

--- DevFramework/DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
+++ DevFramework/DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -1,5 +1,8 @@
 ﻿using DevFramework.Core.Aspects.Postsharp;
+using DevFramework.Core.Aspects.Postsharp.CacheAspects;
+using DevFramework.Core.Aspects.Postsharp.LogAspects;
 using DevFramework.Core.Aspects.Postsharp.TransactionAspects;
+using DevFramework.Core.CrossCuttingConcerns.Caching.Microsoft;
 using DevFramework.Core.CrossCuttingConcerns.Validation.FluentValidation;
 using DevFramework.Northwind.Business.Abstract;
 using DevFramework.Northwind.Business.ValidationRules.FluentValidation;
@@ -7,6 +10,7 @@ using DevFramework.Northwind.DataAccess.Abstract;
 using DevFramework.Northwind.Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.Interception;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,12 +27,17 @@ namespace DevFramework.Northwind.Business.Concrete.Managers
 		}
 
 		[FluentValidationAspect(typeof(ProductValidatior))]
+		[CacheRemoveAspect(typeof(MemoryCacheManager))]
 		public Product Add(Product product)
 		{
 		
 		return _productDal.Add(product);
 		}
 
+
+
+		[CacheAspect(typeof(MemoryCacheManager))]
+		[LogAspect(typeof(DatabaseLogger))]
 		public List<Product> GetAll()
 		{
 			return _productDal.GetList();
